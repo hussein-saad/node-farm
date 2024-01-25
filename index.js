2d2eeae7672a001e5d95a715cbd06292dc16199d
Hussein Saad <hussein.saad2021@gmail.com>
2024-01-25 16:35:00 +0200

    Refactor code to use URL and query parameters

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const http = require('http');
 const fs = require('fs');
 
-const replaceCard = (ele, tempCard) => {
+const replaceTemp = (ele, tempCard) => {
 	let ret = tempCard.replace(/{%IMAGE%}/g, ele.image);
 	ret = ret.replace(/{%PRODUCTNAME%}/g, ele.productName);
 	ret = ret.replace(/{%ID%}/g, ele.id);
 	ret = ret.replace(/{%PRICE%}/g, ele.price);
 	ret = ret.replace(/{%QUANTITY%}/g, ele.quantity);
+	ret = ret.replace(/{%FROM%}/g, ele.form);
+	ret = ret.replace(/{%NUTRIENTS%}/g, ele.nutrients);
+	ret = ret.replace(/{%DESCRIPTION%}/g, ele.description);
+
 	if (!ele.organic) ret = ret.replace('{%NOT_ORGANIC%}', 'not-organic');
 	return ret;
 };
@@ -27,28 +31,36 @@ const fileContent = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const contentObj = JSON.parse(fileContent);
 
 const server = http.createServer((req, res) => {
-	const url = req.url;
+	const myURL = new URL(req.url, `http://${req.headers.host}`);
+	const pathname = myURL.pathname;
+	const query = myURL.searchParams;
 
-	if (url === '/' || url === '/overview') {
+	if (pathname === '/' || pathname === '/overview') {
 		res.writeHead(200, {
 			'Content-type': 'text/html',
 		});
 
-		let cards = contentObj.map((ele) => replaceCard(ele, tempCard)).join('');
+		let cards = contentObj.map((ele) => replaceTemp(ele, tempCard)).join('');
 		let cardsHtml = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cards);
-		
-      res.end(cardsHtml);
+
+		res.end(cardsHtml);
+	} else if (pathname == '/product') {
+		res.writeHead(200, {
+			'Content-type': 'text/html',
+		});
+
+		const product = contentObj[query.get('id')];
+
+		const productHtml = replaceTemp(product, tempProduct);
 	
-   } else if (url == '/product') {
-		res.end('this is products');
+		res.end(productHtml);
 	
-   } else if (url === '/api') {
+   } else if (pathname === '/api') {
 		res.writeHead(200, {
 			'Content-type': 'application/json',
 		});
 		res.end(fileContent);
-	
-   } else {
+	} else {
 		res.writeHead(404, {
 			'Content-type': 'text/html',
 		});
